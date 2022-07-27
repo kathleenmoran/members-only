d412bfa47f636c91c23eda38931e4eab4d9eb896
Kathleen Moran <kathleenmoran10@gmail.com>
2022-07-27 15:15:29 -0400

    Permit username paramater through Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :validatable
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { in: 1..128 }
 
   has_many :posts
 end
